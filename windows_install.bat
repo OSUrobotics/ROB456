675dbcf22cc40bfbed383c31dad5c1837bda6f27
cindygr <cindy.grimm@oregonstate.edu>
2026-03-09 09:47:57 -0700

    Fixing imageio

--- windows_install.bat
+++ windows_install.bat
@@ -2,4 +2,4 @@ winget install astral-sh.uv -h --accept-source-agreements
 uv python install 3.12.7
 uv python pin 3.12.7
 uv init --bare
-uv add numpy scipy matplotlib otter-grader nbconvert ipykernel ipympl
+uv add numpy scipy matplotlib otter-grader nbconvert ipykernel ipympl imageio pip
